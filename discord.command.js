0b7ceed5154f8c6a39ac0530d58a13629094b2bc
himanshuta87 <himanshuta87@gmail.com>
2026-06-21 15:38:56 +0530

    Add files via upload

---
+++ discord.command.js
@@ -0,0 +1,35 @@
+// discord.command.js
+const commands = [
+    {
+        name: 'addtask',
+        description: 'Add a new task to TaskVault',
+        options: [
+            {
+                name: 'question',
+                description: 'The question text',
+                type: 3, // Type 3 means it requires text input
+                required: true
+            },
+            {
+                name: 'answer',
+                description: 'The answer text',
+                type: 3, // Type 3 means it requires text input
+                required: true
+            }
+        ]
+    },
+    {
+        name: 'viewtask',
+        description: 'View your active tasks',
+    },
+    {
+        name: 'myperformance',
+        description: 'Checks your overall performance metrics',
+    },
+    {
+        name: 'stats',
+        description: 'Check your database statistics',
+    }
+];
+
+module.exports = { commands };
